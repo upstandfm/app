728c33e85dd7c4f89b7dae2e9574bcd4fc6d523c
Daniël Illouz <daniel.illouz@me.com>
2019-08-16 12:32:34 +0200

    Setup the app page layout
    Fixes #5

--- src/authenticated-app/AuthenticatedApp.jsx
+++ src/authenticated-app/AuthenticatedApp.jsx
@@ -2,26 +2,40 @@ import React from 'react';
 
 import { useAuth0, useUser } from '../auth0';
 
+import { Container, MenuBar, Brand, Actions, Profile, Main } from './Layout';
+
 function AuthenticatedApp() {
   const { logout } = useAuth0();
   const { fullName } = useUser();
 
   return (
-    <div>
-      <header>
-        <span>upstand.fm</span>
-      </header>
+    <Container>
+      <MenuBar>
+        <Brand>
+          <h3>upstand.fm</h3>
+        </Brand>
 
-      <main>
-        <p>Welcome {fullName}.</p>
+        <Actions>
+          <span>New</span>
+        </Actions>
 
-        <button onClick={logout}>logout</button>
-      </main>
+        <Profile>
+          <span>{fullName}</span>
 
-      <footer>
-        <span>&copy; {new Date().getFullYear()} upstand.fm</span>
-      </footer>
-    </div>
+          <button onClick={logout}>logout</button>
+        </Profile>
+      </MenuBar>
+
+      <Main>
+        <h1>Welcome</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nisi
+          impedit, sed dignissimos asperiores earum possimus, ducimus quam vel
+          quas ad esse ea fuga suscipit eius similique iusto labore! Qui?
+        </p>
+      </Main>
+    </Container>
   );
 }
 
---
+++ src/authenticated-app/Layout.jsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-areas:
+    'menubar main'
+    'menubar main';
+  grid-template-columns: 250px 1fr;
+  grid-template-rows: 1fr;
+
+  @media (max-width: 1000px) {
+    grid-template-areas:
+      'menubar menubar'
+      'main    main';
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr;
+  }
+
+  background-color: lightsalmon;
+`;
+
+export const MenuBar = styled.div`
+  grid-area: menubar;
+  display: grid;
+  grid-template-areas:
+    'brand'
+    'actions'
+    'profile';
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  align-items: center;
+
+  @media (max-width: 1000px) {
+    grid-template-areas: 'brand actions profile';
+    grid-template-columns: auto 1fr auto;
+    grid-template-rows: 1fr;
+  }
+
+  @media (max-width: 550px) {
+    grid-template-areas:
+      'brand   profile'
+      'actions actions';
+    grid-template-columns: 1fr auto;
+    grid-template-rows: 1fr 1fr;
+  }
+
+  background-color: lightpink;
+`;
+
+export const Brand = styled.section`
+  grid-area: brand;
+`;
+
+export const Actions = styled.section`
+  grid-area: actions;
+  margin: 0 auto;
+
+  background-color: lightcyan;
+`;
+
+export const Profile = styled.section`
+  grid-area: profile;
+`;
+
+export const Main = styled.main`
+  grid-area: main;
+  overflow: auto;
+
+  background-color: lightseagreen;
+`;
