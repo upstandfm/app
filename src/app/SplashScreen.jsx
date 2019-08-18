585c956de2643a1b2752c027196a906e058c4159
Daniël Illouz <daniel.illouz@me.com>
2019-08-18 16:14:22 +0200

    Style splash screen
    Fixes #18

--- src/app/SplashScreen.jsx
+++ src/app/SplashScreen.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  background-color: ${props => props.theme.primaryBackgroundColor};
+`;
+
+const Splash = styled.div`
+  margin: auto;
+`;
+
+const Text = styled.h1`
+  text-transform: capitalize;
+  color: ${props => props.theme.primaryColor};
+`;
 
 function SplashScreen() {
   return (
-    <div>
-      <p>Loading App..</p>
-    </div>
+    <Container>
+      <Splash>
+        <Text>loading app..</Text>
+      </Splash>
+    </Container>
   );
 }
 
