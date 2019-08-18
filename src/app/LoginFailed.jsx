6dba9c9dc0e43497b78bf6df2a6ee0d1040eba76
Daniël Illouz <daniel.illouz@me.com>
2019-08-18 22:54:53 +0200

    Extract footer components

--- src/app/LoginFailed.jsx
+++ src/app/LoginFailed.jsx
@@ -5,6 +5,16 @@ import styled from 'styled-components';
 import Button from '../components/Button';
 import Copyright from '../components/Copyright';
 
+import {
+  FOOTER_LINKS_BY_COLUMN,
+  Footer,
+  FooterWrapper,
+  FooterBrand,
+  FooterColumns,
+  FooterColumn,
+  FooterLink
+} from '../components/Footer';
+
 const Container = styled.div`
   height: 100vh;
   font-family: 'Open Sans', sans-serif;
@@ -75,72 +85,6 @@ const PageSection = styled.section`
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.1);
 `;
 
-const Footer = styled.footer`
-  padding: 4em 0;
-  background-color: ${props => props.theme.primaryColor};
-  font-family: 'Nunino', sans-serif;
-`;
-
-const FooterWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 3fr;
-  align-items: center;
-  max-width: 1000px;
-  margin: 0 auto;
-
-  @media (max-width: 780px) {
-    grid-template-columns: 1fr;
-    grid-gap: 3em;
-  }
-`;
-
-const FooterBrand = styled.div`
-  text-align: center;
-  color: var(--color-white);
-`;
-
-const linksByColumn = [
-  [
-    { name: "what's new?", href: '' },
-    { name: 'help', href: '' },
-    { name: 'terms & privacy', href: '' }
-  ],
-  [
-    { name: 'say hi!', href: 'mailto:hi@upstand.fm?subject=Hi there!' },
-    { name: 'about', href: 'https://danillouz.dev' }
-  ],
-  [
-    { name: 'blog', href: 'https://blog.danillouz.dev' },
-    { name: 'GitHub', href: 'https://github.com/upstandfm' }
-  ]
-];
-
-const FooterColumns = styled.div`
-  display: grid;
-  grid-template-columns: ${() => `repeat(${linksByColumn.length}, auto)`};
-
-  @media (max-width: 780px) {
-    grid-template-columns: 1fr;
-    text-align: center;
-  }
-`;
-
-const FooterColumn = styled.div``;
-
-const FooterLink = styled.a`
-  display: block;
-  padding: 0.5em 0;
-  text-transform: capitalize;
-
-  && {
-    color: var(--color-white);
-
-    :visited {
-      color: var(--color-white);
-    }
-  }
-`;
-
 function LoginFailed({ errMessage, handleRetry }) {
   return (
     <Container>
@@ -191,7 +135,7 @@ function LoginFailed({ errMessage, handleRetry }) {
           </FooterBrand>
 
           <FooterColumns>
-            {linksByColumn.map(links => {
+            {FOOTER_LINKS_BY_COLUMN.map(links => {
               return (
                 <FooterColumn>
                   {links.map(link => {
---
+++ src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const FOOTER_LINKS_BY_COLUMN = [
+  [
+    { name: "what's new?", href: '' },
+    { name: 'help', href: '' },
+    { name: 'terms & privacy', href: '' }
+  ],
+  [
+    { name: 'say hi!', href: 'mailto:hi@upstand.fm?subject=Hi there!' },
+    { name: 'about', href: 'https://danillouz.dev' }
+  ],
+  [
+    { name: 'blog', href: 'https://blog.danillouz.dev' },
+    { name: 'GitHub', href: 'https://github.com/upstandfm' }
+  ]
+];
+
+export const Footer = styled.footer`
+  padding: 4em 0;
+  background-color: ${props => props.theme.primaryColor};
+  font-family: 'Nunino', sans-serif;
+`;
+
+export const FooterWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  align-items: center;
+  max-width: 1000px;
+  margin: 0 auto;
+
+  @media (max-width: 780px) {
+    grid-template-columns: 1fr;
+    grid-gap: 3em;
+  }
+`;
+
+export const FooterBrand = styled.div`
+  text-align: center;
+  color: var(--color-white);
+`;
+
+export const FooterColumns = styled.div`
+  display: grid;
+  grid-template-columns: ${() =>
+    `repeat(${FOOTER_LINKS_BY_COLUMN.length}, auto)`};
+
+  @media (max-width: 780px) {
+    grid-template-columns: 1fr;
+    text-align: center;
+  }
+`;
+
+export const FooterColumn = styled.div``;
+
+export const FooterLink = styled.a`
+  display: block;
+  padding: 0.5em 0;
+  text-transform: capitalize;
+
+  && {
+    color: var(--color-white);
+
+    :visited {
+      color: var(--color-white);
+    }
+  }
+`;
