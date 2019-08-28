3ec4736e3f0126192e63505dfcdf84ba83a46891
Daniël Illouz <daniel.illouz@me.com>
2019-08-28 10:34:26 +0200

    Rearrange integration tests

--- cypress/integration/unauthenticated-app/loading.spec.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="Cypress" />
-
-describe('Loading', () => {
-  it('shows splashscreen', () => {
-    cy.visit('/');
-    cy.get('[data-cy="loading"]').should('have.text', 'loading app..');
-  });
-});
