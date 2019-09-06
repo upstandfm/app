033e6ecf9465de24b4890bb6541e436a8bd67329
Daniël Illouz <daniel.illouz@me.com>
2019-09-06 21:43:07 +0200

    Try safari vh fix
    See: https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser

--- src/authenticated-app/Layout.jsx
+++ src/authenticated-app/Layout.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   height: 100vh;
+  min-height: -webkit-fill-available;
   display: grid;
   grid-template-areas:
     'header'
