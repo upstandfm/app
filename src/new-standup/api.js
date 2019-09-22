c46978abb96051829368c5d2ff32b78f35af1f69
Daniël Illouz <daniel.illouz@me.com>
2019-09-22 15:06:44 +0200

    Hookup new standup flow to API
    Fixes #46
    
    Make red + green "lightest" variations even lighter.

---
+++ src/new-standup/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const { REACT_APP_API_DOMAIN } = process.env;
+
+const api = {
+  /**
+   * Create a new standup.
+   *
+   * @param {String} token - Access token
+   * @param {String} cancelToken - Cancellation token to abort the HTTP request
+   * @param {Object} data - Standup data
+   *
+   * @return {Promise} Axios res: https://github.com/axios/axios#response-schema
+   */
+  createStandup(token, cancelToken, data) {
+    return axios({
+      method: 'post',
+      url: `${REACT_APP_API_DOMAIN}/standups`,
+      cancelToken,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      data
+    });
+  }
+};
+
+export default api;
---
+++ src/new-standup/use-create-standup.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+import { useAuth0 } from '../auth0';
+
+import api from './api';
+
+function useCreateStandup() {
+  const { getToken } = useAuth0();
+
+  const [isCreating, setIsCreating] = React.useState(false);
+  const [err, setErr] = React.useState(null);
+
+  const CancelToken = axios.CancelToken;
+  const source = CancelToken.source();
+
+  const createStandup = async standupData => {
+    try {
+      setIsCreating(true);
+      setErr(null);
+
+      const token = await getToken();
+      const res = await api.createStandup(token, source.token, standupData);
+
+      setIsCreating(false);
+
+      return res.data;
+    } catch (err) {
+      if (axios.isCancel(err)) {
+        return;
+      }
+
+      const { response = {} } = err;
+      const { data = {} } = response;
+      setErr(data ? data : err);
+      setIsCreating(false);
+    }
+  };
+
+  return [createStandup, source.cancel, isCreating, err];
+}
+
+export default useCreateStandup;
