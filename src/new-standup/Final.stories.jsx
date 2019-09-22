118bac9f2fa0fd36c8496290d289d1f02ac71709
Daniël Illouz <daniel.illouz@me.com>
2019-09-22 15:57:27 +0200

    Split Final component off into "pure" component

---
+++ src/new-standup/Final.stories.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { PureFinal } from './Final';
+
+const _previousAction = action('handlePrevious');
+const _createAction = action('handleCreate');
+
+const props = {
+  standupUsers: [],
+  handlePrevious(e) {
+    e.preventDefault();
+    _previousAction();
+  },
+  handleCreate(e) {
+    e.preventDefault();
+    _createAction();
+  }
+};
+
+export default {
+  title: 'screens|New Standup/Final',
+  parameters: {
+    componentSubtitle: 'Final step in standup creation'
+  }
+};
+
+export const DefaultFinal = () => {
+  return <PureFinal {...props} />;
+};
+
+DefaultFinal.story = {
+  name: 'default'
+};
+
+export const FinalCreating = () => {
+  return <PureFinal {...props} isCreating={true} />;
+};
+
+FinalCreating.story = {
+  name: 'creating'
+};
+
+export const FinalCreateErr = () => {
+  return (
+    <PureFinal
+      {...props}
+      err={{
+        message: 'Invalid request data',
+        details: ['"standupName" is required']
+      }}
+    />
+  );
+};
+
+FinalCreateErr.story = {
+  name: 'error'
+};
