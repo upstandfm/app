c5b6f4c17021e89301b4917605b50d58bea31817
Daniël Illouz <daniel.illouz@me.com>
2019-10-25 17:13:09 +0200

    Add more loading states to standup updates story

--- src/updates/Updates.stories.jsx
+++ src/updates/Updates.stories.jsx
@@ -5,6 +5,7 @@ import mockData from './mock-data';
 import { PureUpdates } from './Updates';
 
 const playPauseAudio = action('playPauseAudio');
+const fetchMoreUpdates = action('fetchMoreUpdates');
 
 export default {
   title: 'screens|Standup/Updates',
@@ -18,10 +19,19 @@ export const DefaultUpdates = () => {
   return (
     <PureUpdates
       isLoading={false}
+      isLoadingMore={false}
       updates={mockData}
+      audioPlayerState={{
+        playingFile: {
+          fileId: null,
+          fileKey: null
+        },
+        isPlaying: false,
+        downloadProgress: {},
+        files: {}
+      }}
       playPauseAudio={playPauseAudio}
-      playingFileId={'1'}
-      audioPlayerIsPlaying={false}
+      fetchMoreUpdates={fetchMoreUpdates}
     />
   );
 };
@@ -31,9 +41,51 @@ DefaultUpdates.story = {
 };
 
 export const LoadingUpdates = () => {
-  return <PureUpdates isLoading={true} updates={{}} />;
+  return (
+    <PureUpdates
+      isLoading={true}
+      isLoadingMore={false}
+      updates={mockData}
+      audioPlayerState={{
+        playingFile: {
+          fileId: null,
+          fileKey: null
+        },
+        isPlaying: false,
+        downloadProgress: {},
+        files: {}
+      }}
+      playPauseAudio={playPauseAudio}
+      fetchMoreUpdates={fetchMoreUpdates}
+    />
+  );
 };
 
 LoadingUpdates.story = {
   name: 'loading'
+};
+
+export const LoadingMoreUpdates = () => {
+  return (
+    <PureUpdates
+      isLoading={true}
+      isLoadingMore={true}
+      updates={mockData}
+      audioPlayerState={{
+        playingFile: {
+          fileId: null,
+          fileKey: null
+        },
+        isPlaying: false,
+        downloadProgress: {},
+        files: {}
+      }}
+      playPauseAudio={playPauseAudio}
+      fetchMoreUpdates={fetchMoreUpdates}
+    />
+  );
+};
+
+LoadingMoreUpdates.story = {
+  name: 'loading more'
 };
