db4b5e5ecaf7dae7ec7bd9d081f114d51f4a3e2c
Daniël Illouz <daniel.illouz@me.com>
2019-10-31 10:49:17 +0100

    Extract Avatar component

--- src/standup-members/Members.jsx
+++ src/standup-members/Members.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { getFullNameInitials } from './utils';
+import Avatar, { AvatarContainer } from '../components/Avatar';
+
 export const Members = styled.ul`
   display: inline-flex;
   flex-direction: row-reverse;
@@ -13,45 +14,17 @@ export const Members = styled.ul`
 const MemberContainer = styled.li`
   display: inline-block;
   margin-right: -10px;
-
-  :hover {
-    cursor: default;
-  }
-`;
-
-const AvatarContainer = styled.div`
-  position: relative;
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-  background-color: var(--color-lighter-grey);
-  color: var(--color-dark-purple);
-  font-weight: bold;
-  border: 4px solid var(--color-white);
-`;
-
-const Avatar = styled.img`
-  position: absolute;
-  display: ${props => (Boolean(props.src) ? 'block' : 'none')};
-  top: 0;
-  left: 0;
-  border: 0;
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
 `;
 
 export function Member({ userFullName, avatarUrl }) {
-  const initials = getFullNameInitials(userFullName);
   return (
-    <MemberContainer title={userFullName}>
-      <AvatarContainer>
-        {initials}
-        <Avatar src={avatarUrl} alt="standup member avatar" />
-      </AvatarContainer>
+    <MemberContainer>
+      <Avatar
+        title={userFullName}
+        fullName={userFullName}
+        avatarUrl={avatarUrl}
+        altText="standup member avatar image"
+      />
     </MemberContainer>
   );
 }
