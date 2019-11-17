fc4e719e49985e9a52e4eb59d7c400a61cb44d87
Daniël Illouz <daniel.illouz@me.com>
2019-11-17 18:12:13 +0100

    Apply new cards design to all components

--- src/standups/mock-data.js
+++ src/standups/mock-data.js
@@ -4,56 +4,38 @@
 export default [
   {
     standupId: '1',
-    standupName: 'Team awesome',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1499938971550-7ad287075e0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2467&q=80'
+    standupName: 'Team awesome'
   },
   {
     standupId: '2',
-    standupName: 'Pizza destroyers',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&auto=format&fit=crop&w=2555&q=80'
+    standupName: 'Pizza destroyers'
   },
   {
     standupId: '3',
-    standupName: 'Team "ship it!"',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1476058262011-9a78b43bb832?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    standupName: 'Team "ship it!"'
   },
   {
     standupId: '4',
-    standupName: 'Developers developers developers',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1533230408708-8f9f91d1235a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    standupName: 'Developers developers developers'
   },
   {
     standupId: '5',
-    standupName: 'Team kaputmachen',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1517594422361-5eeb8ae275a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2304&q=80'
+    standupName: 'Team kaputmachen'
   },
   {
     standupId: '6',
-    standupName: 'Superduperlongname',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1486428263684-28ec9e4f2584?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    standupName: 'Superduperlongname'
   },
   {
     standupId: '7',
-    standupName: 'AI & ML',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1539606328118-80c679838702?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1164&q=80'
+    standupName: 'AI & ML'
   },
   {
     standupId: '8',
-    standupName: 'Customer success',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1464446066817-4116494586bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    standupName: 'Customer success'
   },
   {
     standupId: '9',
-    standupName: 'Finance',
-    standupImageUrl:
-      'https://images.unsplash.com/photo-1554672723-d42a16e533db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80'
+    standupName: 'Finance'
   }
 ];
