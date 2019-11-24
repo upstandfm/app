c46cd710b565baa4b5c6b9a15ed8b13b819413ff
Daniël Illouz <daniel.illouz@me.com>
2019-11-24 16:09:32 +0100

    Format audio player timing data as MM:SS
    Since we only deal with short audio files of 3 min. max. duration,
    formatting the data with hours is a bit distracting.

--- src/components/AudioPlayer/utils.js
+++ src/components/AudioPlayer/utils.js
@@ -3,12 +3,12 @@
  *
  * @param {Number} - Number in seconds to format
  *
- * @return {String} Formatted string: "HH:MM:SS"
+ * @return {String} Formatted string: "MM:SS"
  */
 export function formatTime(sec = 0) {
   if (!sec) {
-    return '00:00:00';
+    return '00:00';
   }
 
-  return new Date(sec * 1e3).toISOString().substr(11, 8);
+  return new Date(sec * 1e3).toISOString().substr(14, 5);
 }
