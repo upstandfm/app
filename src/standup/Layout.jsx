2cfc27377e161c7660e91e09cef475be40ec003b
Daniël Illouz <daniel.illouz@me.com>
2019-11-24 16:03:37 +0100

    Keep audio player visible
    Fixes #84
    
    - Make updates container "scrollable"
    - Simplify media queries

--- src/standup/Layout.jsx
+++ src/standup/Layout.jsx
@@ -10,16 +10,7 @@ export const Container = styled.div`
     'standup-info   standup-updates';
 
   @media (max-width: 770px) {
-    grid-template-columns: 1fr;
-    grid-template-rows: auto auto 1fr;
-    grid-template-areas:
-      'standup-player'
-      'standup-info'
-      'standup-updates';
-  }
-
-  @media (max-width: 570px) {
-    height: calc(100vh - 112px);
+    height: calc(100vh - 72px);
     grid-template-columns: 1fr;
     grid-template-rows: auto 1fr auto;
     grid-template-areas:
@@ -27,28 +18,37 @@ export const Container = styled.div`
       'standup-updates'
       'standup-player';
   }
+
+  @media (max-width: 570px) {
+    height: calc(100vh - 112px);
+  }
 `;
 
 export const StandupPlayer = styled.div`
   grid-area: standup-player;
   display: grid;
   justify-items: center;
-  border-bottom: 1px solid var(--color-light-grey);
-  margin: 0 0 1em 0;
+  box-shadow: 0 3px 3px -3px rgba(0, 0, 0, 0.2);
+  margin: 00;
   padding: 1em 0;
   width: 100%;
+  z-index: 1;
 
-  @media (max-width: 570px) {
-    border-top: 1px solid var(--color-lighter-grey);
-    border-bottom: none;
-    margin: 1em 0 0 0;
+  @media (max-width: 770px) {
+    box-shadow: 0 -3px 3px -3px rgba(0, 0, 0, 0.2);
   }
 `;
 
 export const StandupInfo = styled.div`
   grid-area: standup-info;
+
+  @media (max-width: 770px) {
+    box-shadow: 0 3px 3px -3px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+  }
 `;
 
 export const StandupUpdates = styled.div`
   grid-area: standup-updates;
+  overflow: auto;
 `;
