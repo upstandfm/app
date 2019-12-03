a95e7d259ed11e1998a0d54d00ee0dcb2968b202
Daniël Illouz <daniel.illouz@me.com>
2019-12-03 16:20:45 +0100

    Make updates date header sticky

--- src/updates/Layout.jsx
+++ src/updates/Layout.jsx
@@ -7,19 +7,55 @@ export const Container = styled.div`
   padding: 1em;
 `;
 
-export const Subtitle = styled.h2`
-  display: inline-block;
-  margin: 0;
-  font-weight: normal;
+export const DayDivider = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+`;
+
+export const Day = styled.hr`
+  height: 24px;
+  margin: 0 0 2em 0;
+  padding: 0;
+  border: none;
+  border-bottom: 1px solid var(--color-light-grey);
+  overflow: visible;
+  text-align: center;
+  background-color: var(--color-lightest-grey);
+
+  :after {
+    position: relative;
+    top: 13px;
+    background-color: var(--color-lightest-grey);
+    color: var(--color-darkest-purple);
+    border-radius: 33px;
+    padding: 0.25em 0.75em;
+
+    /* The content value MUST be within quotes! */
+    content: '${props => props.formattedDate}';
+  }
 
   :hover {
     cursor: ${props => (props.showHelp ? 'help' : 'inherit')};
   }
 `;
 
-Subtitle.propTypes = {
+Day.propTypes = {
+  formattedDate: PropTypes.string.isRequired,
   showHelp: PropTypes.bool
 };
+
+const Subtitle = styled.h4`
+  display: inline-block;
+  margin: 0;
+  padding: 0.5em 1em;
+  border-radius: var(--border-size);
+  background-color: var(--color-lightest-purple);
+
+  :hover {
+    cursor: ${props => (props.showHelp ? 'help' : 'inherit')};
+  }
+`;
 
 const glimmer = keyframes`
   0% {
