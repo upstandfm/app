7089340fcc69732e12ed9af35aa287b194709a32
Daniël Illouz <daniel.illouz@me.com>
2019-12-23 15:45:44 +0100

    Simplify layout and show audio player in new update page

--- src/new-update/Layout.jsx
+++ src/new-update/Layout.jsx
@@ -1,23 +1,50 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 50rem;
-  margin: auto;
-  padding: 1em;
+  display: grid;
+  height: calc(100vh - 55px);
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    'new-standup-header'
+    'new-standup-main'
+    'new-standup-player';
 `;
 
 export const Header = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto;
-  grid-gap: 2em;
-  align-items: center;
+  grid-area: new-standup-header;
+  text-align: center;
+  position: absolute;
+  top: 0.5em;
+  left: 0;
+  right: 0;
+
+  @media (max-width: 480px) {
+    display: grid;
+    grid-template-rows: auto auto;
+    grid-gap: 0.5em;
+    align-items: center;
+    justify-items: center;
+    position: static;
+  }
 `;
 
 export const Main = styled.div`
-  margin: 0;
-  padding: 0;
+  grid-area: new-standup-main;
   display: grid;
-  grid-gap: 3em;
+`;
+
+export const Wrapper = styled.div`
+  margin: auto;
+`;
+
+export const Player = styled.div`
+  grid-area: new-standup-player;
+  box-shadow: 0 -3px 3px -3px rgba(0, 0, 0, 0.2);
+  background-color: var(--color-white);
+  margin: 0;
+  padding: 1em 0;
+  z-index: 1;
 `;
 
 export const Actions = styled.div`
