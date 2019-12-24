a330eb2d8abe8d741a19f5b88e760bf5a3a3567a
Daniël Illouz <daniel.illouz@me.com>
2019-12-24 11:58:11 +0100

    Extract NewUpdate "core" logic to "pure" component
    - Give error details view a max width
    - Create stories for basic new update states

---
+++ src/new-update/NewUpdate.stories.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { SnackbarProvider } from '../components/Snackbar';
+
+import { PureNewUpdate } from './NewUpdate';
+
+const handleGetPermission = action('handleGetPermission');
+const onNewRecording = action('onNewRecording');
+const onDeleteUpdate = action('onDeleteUpdate');
+const handlePublish = action('handlePublish');
+const onUploadedFile = action('onUploadedFile');
+
+export default {
+  title: 'screens|NewUpdate',
+  component: PureNewUpdate,
+  parameters: {
+    componentSubtitle: 'Screen to record and publish a new update'
+  }
+};
+
+export const DefaultNewUpdate = () => {
+  return (
+    <PureNewUpdate
+      userMediaStream={undefined}
+      isGettingPermission={false}
+      permissionErr={undefined}
+      handleGetPermission={handleGetPermission}
+      standupId={'1a2z3x'}
+      updatesState={{}}
+      isPublishing={false}
+      onNewRecording={onNewRecording}
+      onDeleteUpdate={onDeleteUpdate}
+      handlePublish={handlePublish}
+      onUploadedFile={onUploadedFile}
+    />
+  );
+};
+
+DefaultNewUpdate.story = {
+  name: 'default'
+};
+
+export const NewUpdatePermission = () => {
+  return (
+    <PureNewUpdate
+      userMediaStream={undefined}
+      isGettingPermission={true}
+      permissionErr={undefined}
+      handleGetPermission={handleGetPermission}
+      standupId={'1a2z3x'}
+      updatesState={{}}
+      isPublishing={false}
+      onNewRecording={onNewRecording}
+      onDeleteUpdate={onDeleteUpdate}
+      handlePublish={handlePublish}
+      onUploadedFile={onUploadedFile}
+    />
+  );
+};
+
+NewUpdatePermission.story = {
+  name: 'get permission'
+};
+
+export const NewUpdatePermissionErr = () => {
+  return (
+    <SnackbarProvider>
+      <PureNewUpdate
+        userMediaStream={undefined}
+        isGettingPermission={false}
+        permissionErr={new Error()}
+        handleGetPermission={handleGetPermission}
+        standupId={'1a2z3x'}
+        updatesState={{}}
+        isPublishing={false}
+        onNewRecording={onNewRecording}
+        onDeleteUpdate={onDeleteUpdate}
+        handlePublish={handlePublish}
+        onUploadedFile={onUploadedFile}
+      />
+    </SnackbarProvider>
+  );
+};
+
+NewUpdatePermissionErr.story = {
+  name: 'permission error'
+};
+
+export const NewUpdateMediaStream = () => {
+  return (
+    <SnackbarProvider>
+      <PureNewUpdate
+        userMediaStream={new MediaStream()}
+        isGettingPermission={false}
+        permissionErr={undefined}
+        handleGetPermission={handleGetPermission}
+        standupId={'1a2z3x'}
+        updatesState={{}}
+        isPublishing={false}
+        onNewRecording={onNewRecording}
+        onDeleteUpdate={onDeleteUpdate}
+        handlePublish={handlePublish}
+        onUploadedFile={onUploadedFile}
+      />
+    </SnackbarProvider>
+  );
+};
+
+NewUpdateMediaStream.story = {
+  name: 'media stream'
+};
