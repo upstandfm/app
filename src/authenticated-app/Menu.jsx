ac472bf8255b4fae96cac2c515a7f7fd083938c6
Daniël Illouz <daniel.illouz@me.com>
2019-12-31 19:21:40 +0100

    Tweak menu spacing and active styles

--- src/authenticated-app/Menu.jsx
+++ src/authenticated-app/Menu.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from '@reach/router';
 
-export const Menu = styled.div`
-  margin: 2em 0;
-`;
+export const Menu = styled.div``;
 
 const StyledLink = styled(Link)`
   display: grid;
@@ -12,13 +10,13 @@ const StyledLink = styled(Link)`
   grid-gap: 0.75em;
   align-items: center;
   margin: 0;
-  padding: 0.1em 1em 0.1em 1.5em;
+  padding: 0.1em 1em;
   text-decoration: none;
   min-height: 27px;
   transition: all 0.1s linear;
 
   :hover {
-    background-color: var(--color-lighter-purple);
+    background-color: var(--color-lightest-purple);
     color: var(--color-darkest-purple) !important;
   }
 `;
@@ -33,8 +31,7 @@ export const MenuLink = props => (
 
       const activeStyles = {
         color: 'var(--color-darkest-purple)',
-        backgroundColor: 'var(--color-lighter-purple)',
-        fontWeight: 'bold'
+        backgroundColor: 'var(--color-lighter-purple)'
       };
 
       return {
