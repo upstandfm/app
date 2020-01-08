9df66ad5bb810978a41f9039a6ea7da342a8020f
Daniël Illouz <daniel.illouz@me.com>
2020-01-08 15:37:08 +0100

    Remove "duplicate" layout components
    This will make it possible to "inject" the module as a "child" in the
    app's main router.

--- src/new-update/Layout.jsx
+++ src/new-update/Layout.jsx
@@ -4,48 +4,11 @@ import { List, ListTitle, ListItemText } from '../components/List';
 
 export const Container = styled.div`
   display: grid;
-  height: 100vh;
-  grid-template-columns: 1fr;
-  grid-template-rows: auto 1fr auto;
-  grid-template-areas:
-    'new-standup-header'
-    'new-standup-main'
-    'new-standup-player';
-`;
-
-export const Header = styled.div`
-  grid-area: new-standup-header;
-  padding: 0.25em 0;
-  text-align: center;
-`;
-
-export const Main = styled.div`
-  grid-area: new-standup-main;
-  display: grid;
 `;
 
 export const Wrapper = styled.div`
   margin: auto;
   padding: 1em;
-`;
-
-export const Player = styled.div`
-  grid-area: new-standup-player;
-  box-shadow: 0 -3px 3px -3px rgba(0, 0, 0, 0.2);
-  background-color: var(--color-white);
-  margin: 0;
-  padding: 1em;
-  z-index: 1;
-`;
-
-export const Actions = styled.div`
-  display: grid;
-  justify-items: left;
-  margin: 2em 0 0 0;
-
-  @media (max-width: 470px) {
-    justify-items: center;
-  }
 `;
 
 export const Preview = styled.div`
@@ -58,6 +21,16 @@ export const Preview = styled.div`
 
   @media (max-width: 350px) {
     width: 100%;
+  }
+`;
+
+export const Actions = styled.div`
+  display: grid;
+  justify-items: left;
+  margin: 2em 0 0 0;
+
+  @media (max-width: 470px) {
+    justify-items: center;
   }
 `;
 
