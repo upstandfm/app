175c6f971ce63a8c0e4eeb5537b93d31372c9627
Daniël Illouz <daniel.illouz@me.com>
2020-01-08 15:53:52 +0100

    Extract Standup loading skeleton to own component

---
+++ src/standup/Loading.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { LoadingAudioPlayer } from '../components/AudioPlayer';
+import Button from '../components/Button';
+
+import {
+  Breadcrumbs,
+  Breadcrumb,
+  LoadingBreadcrumb
+} from '../components/Breadcrumbs';
+
+import { LoadingStandupMembers } from '../standup-members';
+
+import { Container, Footer, Header, Main } from './Layout';
+
+function Loading({ children }) {
+  return (
+    <Container>
+      <Header>
+        <Breadcrumbs>
+          <Breadcrumb>Standups</Breadcrumb>
+          <LoadingBreadcrumb />
+        </Breadcrumbs>
+
+        <LoadingStandupMembers />
+
+        <div>
+          <Button tertiary as={Link} to="new-update">
+            New update
+          </Button>
+
+          <Button tertiary disabled title="not implemented yet">
+            <FontAwesomeIcon icon="ellipsis-h" />
+          </Button>
+        </div>
+      </Header>
+
+      <Main>{children}</Main>
+
+      <Footer>
+        <LoadingAudioPlayer />
+      </Footer>
+    </Container>
+  );
+}
+
+export default Loading;
