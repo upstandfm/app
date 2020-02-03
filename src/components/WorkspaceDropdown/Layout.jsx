c6d1a27441d978b46207da5aab3d1a166cfe644a
Daniël Illouz <daniel.illouz@me.com>
2020-02-03 20:14:39 +0100

    Create workspace dropdown component

---
+++ src/components/WorkspaceDropdown/Layout.jsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const Trigger = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 0.5em;
+  align-items: center;
+  width: 220px;
+`;
+
+export const WorkspaceName = styled.h3`
+  margin: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: var(--color-dark-grey);
+`;
+
+export const Icon = styled(FontAwesomeIcon)`
+  color: var(--color-grey);
+`;
+
+export const User = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 0.5em;
+  align-items: center;
+  padding: 8px 14px;
+`;
+
+export const Avatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+export const UserInfo = styled.div`
+  width: 150px;
+`;
+
+const Text = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const FullName = styled(Text)`
+  color: var(--color-dark-grey);
+`;
+
+export const Email = styled(Text)`
+  color: var(--color-grey);
+`;
+
+export const BlankButton = styled.button`
+  box-sizing: border-box;
+  background: none;
+  border: none;
+  font-size: 1em;
+  text-align: left;
+  margin: 0;
+  padding: 8px 14px;
+  width: 100%;
+  display: block;
+  outline: 0;
+
+  :hover {
+    cursor: pointer;
+    background-color: var(--color-light-grey);
+  }
+`;
---
+++ src/components/WorkspaceDropdown/Loading.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import { Trigger, WorkspaceName } from './Layout';
+
+const glimmer = keyframes`
+  0% {
+    background-position: -235px 0;
+  }
+  100% {
+    background-position: calc(235px + 100%) 0;
+  }
+`;
+
+const Container = styled.div`
+  font-size: 1em;
+  line-height: 1;
+`;
+
+const Loading = styled(WorkspaceName)`
+  color: transparent;
+  background-color: var(--color-light-grey);
+  border-radius: var(--radius-size);
+  background-repeat: no-repeat;
+  background-image: linear-gradient(
+    90deg,
+    var(--color-light-grey),
+    var(--color-lighter-grey),
+    var(--color-light-grey)
+  );
+  background-size: 100% 100%;
+  animation: ${glimmer} 1s ease-in-out infinite;
+`;
+
+export default function LoadingWorkspace() {
+  return (
+    <Container>
+      <Trigger>
+        <Loading>A loading tite</Loading>
+      </Trigger>
+    </Container>
+  );
+}
