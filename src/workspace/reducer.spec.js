dcbcaecc36efcda34d618b32042cf9969196fe2c
Daniël Illouz <daniel.illouz@me.com>
2020-02-04 16:55:30 +0100

    Let workspace reducer control the fetch state
    This way we have a single "controller" that's in charge of setting
    all workspace related state. This prevents potential "data races"
    when rendering components that rely on workspace data.

--- src/workspace/reducer.spec.js
+++ src/workspace/reducer.spec.js
@@ -1,9 +1,18 @@
-import reducer from './reducer';
+import reducer, { defaultWorkspaceState } from './reducer';
 
 describe('workspace reducer', () => {
-  it('returns fetched workspace', () => {
-    const state = {};
+  it('sets loading state', () => {
+    const action = {
+      type: 'FETCHING_WORKSPACE',
+      data: {}
+    };
 
+    const newState = reducer(defaultWorkspaceState, action);
+    expect(newState.isFetching).toEqual(true);
+    expect(newState.fetchErr).toEqual(null);
+  });
+
+  it('returns fetched workspace + updates loading- and error state', () => {
     const now = new Date().toISOString();
     const workspace = {
       id: 'xs0Yt56',
@@ -19,27 +28,33 @@ describe('workspace reducer', () => {
       data: workspace
     };
 
-    const newState = reducer(state, action);
-    expect(newState).toEqual(workspace);
+    const newState = reducer(defaultWorkspaceState, action);
+    expect(newState).toEqual({
+      isFetching: false,
+      fetchErr: null,
+      ...workspace
+    });
+  });
+
+  it('sets error state', () => {
+    const action = {
+      type: 'FETCH_WORKSPACE_ERROR',
+      data: new Error('Boom!')
+    };
+
+    const newState = reducer(defaultWorkspaceState, action);
+    expect(newState.isFetching).toEqual(false);
+    expect(newState.fetchErr).toEqual(action.data);
   });
 
   it('returns default state', () => {
-    const now = new Date().toISOString();
-    const state = {
-      id: 'xs0Yt56',
-      createdBy: 'user|564738575e89e38f47',
-      createdAt: now,
-      updatedAt: now,
-      name: 'My workspace',
-      slug: 'my-workspace'
-    };
-
     const action = {
       type: 'DOES_NOT_EXIST',
       data: {}
     };
 
-    const newState = reducer(state, action);
-    expect(newState).toEqual(state);
+    const newState = reducer(defaultWorkspaceState, action);
+    expect(newState).toEqual(defaultWorkspaceState);
+    expect(newState.isFetching).toEqual(true);
   });
 });
