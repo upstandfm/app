d5ffde5d174729634ced0f3b80720528a6f46ac8
Daniël Illouz <daniel.illouz@me.com>
2020-02-04 15:24:33 +0100

    Expand Content component to more easily create content pages

--- src/components/Content/Content.stories.jsx
+++ src/components/Content/Content.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Content from './Content';
+import Content, { Title, Subtitle, Section, SectionTitle } from './Content';
 
 export default {
   title: 'components/Content',
@@ -12,19 +12,126 @@ export default {
 
 export const DefaultContent = () => {
   return (
-    <Content title="This is a title" subtitle="And this is a subtitle.">
-      <p>Pass the main content as child components.</p>
+    <Content>
+      <Title>This is a title</Title>
+      <Subtitle>And this is a subtitle</Subtitle>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis, harum
-        magnam nesciunt, architecto facere nulla sit similique explicabo ducimus
-        eveniet iusto alias! Fugiat provident eveniet maiores odit architecto
-        reprehenderit!
-      </p>
+      <Section>
+        <p>Pass the main content as child components.</p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+      </Section>
     </Content>
   );
 };
 
 DefaultContent.story = {
   name: 'title + subtitle + children'
+};
+
+export const ContentSections = () => {
+  return (
+    <Content>
+      <Title>This is a title</Title>
+      <Subtitle>And this is a subtitle</Subtitle>
+
+      <Section>
+        <p>You can add multiple sections for more semantic grouping.</p>
+      </Section>
+
+      <Section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+      </Section>
+
+      <Section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+      </Section>
+    </Content>
+  );
+};
+
+ContentSections.story = {
+  name: 'sections'
+};
+
+export const ContentSectionSubtitles = () => {
+  return (
+    <Content>
+      <Title>This is a title</Title>
+      <Subtitle>And this is a subtitle</Subtitle>
+
+      <Section>
+        <p>Sections can have subtitles.</p>
+      </Section>
+
+      <Section>
+        <SectionTitle>Subtitle One</SectionTitle>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+      </Section>
+
+      <Section>
+        <SectionTitle>Subtitle Two</SectionTitle>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nobis,
+          harum magnam nesciunt, architecto facere nulla sit similique explicabo
+          ducimus eveniet iusto alias! Fugiat provident eveniet maiores odit
+          architecto reprehenderit!
+        </p>
+      </Section>
+    </Content>
+  );
+};
+
+ContentSectionSubtitles.story = {
+  name: 'section subtitles'
 };
