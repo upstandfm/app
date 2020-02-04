9e41670e69a584a9e83f84565dd9445b18733841
Daniël Illouz <daniel.illouz@me.com>
2020-02-04 15:24:33 +0100

    Expand Content component to more easily create content pages

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,48 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  max-width: 850px;
-  margin: 0 auto;
-`;
+const Container = styled.div``;
 
-const Title = styled.h1`
+export const Title = styled.h2`
   margin: 0;
-  padding: 0.5em 0 0 0;
+  padding: 0;
 `;
 
-const Subtitle = styled.h2`
+export const Subtitle = styled.h3`
   margin: 0;
   padding: 0;
   font-weight: normal;
+  color: var(--color-dark-grey);
 `;
 
-const Section = styled.section`
-  margin: 1.5em 0 0 0;
-  padding: 1.5em;
+export const Section = styled.section`
+  margin: 1em 0 1.5em 0;
+  padding: 1em;
   background-color: var(--color-white);
   border-radius: var(--radius-size);
   box-shadow: 0px 1px 2px 0px rgba(25, 18, 56, 0.18);
+`;
+
+export const SectionTitle = styled.h3`
+  margin: 0;
+  padding-bottom: 0.5em;
+  border-bottom: 1px solid var(--color-light-grey);
 `;
 
 /**
  * Primary page content. You can pass one or more components as children.
  */
 function Content(props) {
-  const { title, subtitle } = props;
-
-  return (
-    <Container>
-      <Title>{title}</Title>
-      <Subtitle>{subtitle}</Subtitle>
-      <Section>{props.children}</Section>
-    </Container>
-  );
+  return <Container>{props.children}</Container>;
 }
 
 Content.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
 };
 
 export default Content;
